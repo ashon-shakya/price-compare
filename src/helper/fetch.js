4d2677a644ff7fedbf7fa4d3492aae80648f7834
Ashon Shakya <shakya.ashon@gmail.com>
2026-01-22 14:06:26 +1100

    update: bypass warning

--- src/helper/fetch.js
+++ src/helper/fetch.js
@@ -3,6 +3,10 @@ const baseUrl = import.meta.env.VITE_BASE_URL || "http://localhost:5000";
 export const fetchRecordsApi = async (queryTerm) => {
   let response = await fetch(`${baseUrl}/api/v1/price/all/${queryTerm}`, {
     method: "get",
+    headers: {
+      "ngrok-skip-browser-warning": "true", // often used for ngrok
+      "X-Tunnel-Skip-Anti-Phishing-Page": "1", // specifically for MS Dev Tunnels
+    },
   });
 
   return await response.json();
